8028d3650b95c5d32630ed8051c30c4e0b3d8dd4
carolinabranquinho <carol.a.branquinho@gmail.com>
2020-06-08 16:06:44 -0300

    Adding dark mode to the home page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,9 @@
 const buttonSearch = document.querySelector("#page-home main a");
 const modal = document.querySelector("#modal");
 const close = document.querySelector("#modal .header a");
-const night_mode_button = document.querySelector(".night_mode_button button");
-const night_mode = document.querySelector(".night_mode");
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+//const night_mode = document.querySelector(".night_mode");
 
 buttonSearch.addEventListener("click", () => {      
     modal.classList.remove("hide");
@@ -12,6 +13,26 @@ close.addEventListener("click", () =>{
     modal.classList.add("hide");
 })
 
-night_mode_button.addEventListener("click", () => {
-    night_mode.classList.toggle("night_mode_button")
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkMode", "true");
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkMode", null);
+}
+
+if(darkMode === "true"){
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+
+    if(darkMode !== 'true'){
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 })
